4bb0f8c7e8c3711b48311bc168d7d80d2fd9cd0c
Tejora <numan.992@gmail.com>
2023-01-20 19:56:29 +0530

    Task commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace CheckIfNumber
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter some text");
+            string text = Console.ReadLine();
+            bool result = IsNumber(text);
+            if (result)
+            {
+                Console.WriteLine("Text Entered is Number");
+            }
+            else
+            {
+                Console.WriteLine("Text Entered is not a Number");
+            }
+        }
+
+        public static bool IsNumber(string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                if ((text[i] < '0') || (text[i] > '9'))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
